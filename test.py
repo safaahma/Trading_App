fda83455d24f2c9fa4a15df9ac82838633f69398
sadhathameed <saddu.hameed@gmail.com>
2023-06-28 12:21:29 +0530

    Creation of Home.html and implementing render_template

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+dataList = [{'id': 1, 'name': 'Sadhat'}, {'id': 2, 'name': 'Hameed'}]
+
+
 @app.route('/')
 def hello_world():
-    return '<p><b>Hello Abdul Ahad</b></p>'
+    return render_template('home.html', jobs=dataList)
 
 
 if __name__ == '__main__':
